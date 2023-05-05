849e67855ec7b018b4b2317d2b2db05153c0f36e
Shumpei Shiina <shiina@eidos.ic.i.u-tokyo.ac.jp>
2023-05-05 20:01:00 +0900

    Update Open MPI settings

--- run_common.bash
+++ run_common.bash
@@ -83,6 +83,7 @@ case $KOCHI_MACHINE in
       trap "compgen -G ${STDOUT_FILE}.* && tail -n +1 \$(ls ${STDOUT_FILE}.* -v) > $STDOUT_FILE && rm ${STDOUT_FILE}.*" EXIT
 
       $MPIEXEC -n $n_processes -N $n_processes_per_node \
+        --bind-to core \
         --output file=$STDOUT_FILE \
         --prtemca ras simulator \
         --prtemca plm_ssh_agent ssh \
